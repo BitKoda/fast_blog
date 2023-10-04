76ba745185f45b90a75024a2d115e6ec83b83794
bitkoda <jf.liv@pm.me>
2023-10-04 16:36:09 +0100

    add show view and test for same

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -12,4 +12,12 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2', 'The Fast 800 Blogger'
     assert_select 'h3 a', @article.title.titleize
   end
+
+  test 'should get show page' do
+    get article_path(@article)
+    assert_response :success
+    assert_select 'h1', @article.title.titleize
+    assert_select 'p.text-xl', @article.subtitle
+    assert_select 'p', @article.content
+  end
 end
